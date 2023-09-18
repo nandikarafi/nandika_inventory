de7fbf42a1e9969b252c70cbf5aab1854ebd5bde
nandikarafi <nandikarafi@gmail.com>
2023-09-18 19:19:02 +0700

    ulai mengerjakan tugas3

--- main/views.py
+++ main/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Tas Sekolah',
-        'harga': 'Rp. 50.000',
-        'description': 'Tas merupakan jenis ransel yang diproduksi di Indonesia\nTas memiliki banyak varian warna\nTerdapat bonus berupa jas hujan sebagai pelindung tas dari hujan ',
-        'amount': 25,
+        'name': 'Nandika Rafi Atallah',
+        'kelas': 'PBP C',
     }
 
     return render(request, "main.html", context)
